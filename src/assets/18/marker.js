7136d46660f880be12dab25b017b9377a9e8361f
Ivan Driuk <veytarn@gmail.com>
2018-03-13 16:13:38 +0200

    make functional components for icons

--- src/assets/18/marker.js
+++ src/assets/18/marker.js
@@ -1,3 +1,6 @@
-<svg id="18x18_marker" height="100%" viewBox="0 0 18 18" width="100%">
+import React from 'react';
+
+export default () =>
+<svg className="icon_svg" id="18x18_marker" height="100%" viewBox="0 0 18 18" width="100%">
   <path d="M14 7A5 5 0 0 0 4 7c0 1.97 1.15 3.658 2.806 4.472h-.17L9 16l2.363-4.528h-.17C12.85 10.658 14 8.97 14 7zM9 5a2 2 0 1 1 0 4 2 2 0 0 1 0-4z"></path>
 </svg>
--- src/assets/24/nearby_outline.js
+++ src/assets/24/nearby_outline.js
@@ -1,3 +1,6 @@
-<svg id="24x24_nearby_outline" height="100%" viewBox="0 0 24 24" width="100%">
+import React from 'react';
+
+export default () =>
+<svg className="icon_svg" id="24x24_nearby_outline" height="100%" viewBox="0 0 24 24" width="100%">
   <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-19c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9zm-1.034 10.084l-3.96-.364 8.888-4.564-4.564 8.888-.364-3.96z"></path>
 </svg>
